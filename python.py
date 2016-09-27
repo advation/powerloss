7240d0d677b41f16377a25b13158278ce49b414c
Adam <advation@users.noreply.github.com>
2016-09-27 11:59:14 -0600

    Update python.py
    Multiple accounts can be emailed for a notification.

--- python.py
+++ python.py
@@ -1,23 +1,28 @@
 import serial, time, datetime, os
 
 ser = serial.Serial('/dev/ttyACM0',9600)
-status = 0;
+status = 0
+powerOutNotice = 0
 currentTime = time.time()
 triggerTime = currentTime + 60
-
 while True:
         payload = ser.readline()
         try:
                 payload = int(ser.readline())
                 if payload == 0:
-                        print "Power is out"
+                        if powerOutNotice == 0:
+                                print "Power outage detected"
+                                powerOutNotice = 1
                         if time.time() > triggerTime:
-                                print "Sending email"
-                                string = "MESSAGE HERE %s' | mailx -s 'SUBJECT HERE' email@address.com" % datet$
-                                os.system(string)
-                                #Sets the next email to be sent after 5 minutes.
+                                emails = ["email@address.com"]
+                                for account in emails:
+                                        string = "echo 'Power loss detected at <LOCATION> %s' | mailx -s '<LOCATION>: POWER LOSS' %s" % (datetime.date$
+                                        os.system(string)
+                                        print "Notification sent to %s" % account
+                                #Wait 5 minutes before sending another message
                                 triggerTime = time.time() + 300
                 else:
                         print payload
+                        powerOutNotice = 0;
         except:
-                pass
+                print "Oh no... something's wrong"
